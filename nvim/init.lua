00e5c198f70bd07e5facb1d1ff3c794def65036c
iwsknj <kenji.iwase.5@gmail.com>
2023-12-06 00:59:03 +0900

    refactor:

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,14 @@
 -- refference:
   -- https://github.com/disk-inue/dotfiles/tree/work-article
   -- https://zenn.dev/monicle/articles/59ff479ae51c66
+  -- https://github.com/craftzdog/dotfiles-public/tree/master
 
 vim.loader.enable()
 require 'options'
 require 'keybinds'
-require 'appearance'
+require 'autocmds'
 require 'extensions'
-require 'color_scheme' -- テーマをプラグインとして読み込んでるから読み込みが終わってから読み込む
+require 'appearance' -- テーマをプラグインとして読み込んでるから読み込みが終わってから読み込む
 
 -- netrw（デフォルトのファイラー）を読み込まない
 vim.api.nvim_set_var('loaded_netrw', 1)
--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,16 +1,46 @@
--- インサートモード抜ける
-vim.keymap.set('i', 'jj', '<ESC>', { silent = true})
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
 
 -- 表示行単位で上下移動するように
-vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set('n', 'k', 'gk')
-vim.keymap.set('n', '<Down>', 'gj')
-vim.keymap.set('n', '<Up>', 'gk')
+keymap.set('n', 'j', 'gj')
+keymap.set('n', 'k', 'gk')
+keymap.set('n', '<Down>', 'gj')
+keymap.set('n', '<Up>', 'gk')
 
 -- 逆に普通の行単位で移動したい時のために逆の map も設定しておく
-vim.keymap.set('n', 'gj', 'j')
-vim.keymap.set('n', 'gk', 'k')
+keymap.set('n', 'gj', 'j')
+keymap.set('n', 'gk', 'k')
 
-vim.keymap.set('n', 'Y', 'y$', { noremap = true })
+-- 数字のインクリメント・デクリメント
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
 
+-- タブ
+keymap.set("n", "te", ":tabedit") -- 開く
+keymap.set("n", "<tab>", ":tabnext<Return>", opts) -- 次のタブ
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts) -- 前のタブ
 
+-- 画面分割
+keymap.set("n", "ss", ":split<Return>", opts) -- 水平分割
+keymap.set("n", "sv", ":vsplit<Return>", opts) -- 垂直分割
+
+-- 画面移動
+keymap.set("n", "sh", "<C-w>h") -- 左
+keymap.set("n", "sk", "<C-w>k") -- 上
+keymap.set("n", "sj", "<C-w>j") -- 下
+keymap.set("n", "sl", "<C-w>l") -- 右
+
+-- 画面サイズ調整
+keymap.set("n", "<C-w><left>", "<C-w><") -- 左
+keymap.set("n", "<C-w><right>", "<C-w>>") -- 右
+keymap.set("n", "<C-w><up>", "<C-w>+") -- 上
+keymap.set("n", "<C-w><down>", "<C-w>-") -- 下
+
+keymap.set("n", "x", '"_x') -- xで削除してもクリップボードに入れない
+keymap.set('i', 'jj', '<ESC>', { silent = true}) -- jjでインサートモードを抜ける
+keymap.set('n', 'Y', 'y$', { noremap = true }) -- Yで行末までヤンクする
+keymap.set("n", "dw", 'vb"_d') -- 単語をバックスペース方向に削除するショートカット
+keymap.set("n", "<C-a>", "gg<S-v>G") -- 全選択
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts) -- 新しい行を追加し、インデントなしで上の行に移動する
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts) -- 新しい行を追加し、インデントなしで下の行に移動する
