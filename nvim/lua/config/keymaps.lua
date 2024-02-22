2245f9b7abbdb5fbbffbb64aca44acfeb6a8d6fb
iwsknj <kenji.iwase.5@gmail.com>
2024-02-22 14:22:34 +0900

    mod: tmuxでのコピー共有設定

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,10 @@ discipline.cowboy()
 
 local keymap = vim.keymap
 keymap.set("i", "jj", "<ESC>", { noremap = true, desc = "jjでインサートモードを抜ける" })
+
+keymap.set(
+  "n",
+  "+",
+  "<Cmd>let @+ = @@<CR>",
+  { noremap = true, desc = "レジスタ+に無名レジスタの文字列をコピー" }
+)
