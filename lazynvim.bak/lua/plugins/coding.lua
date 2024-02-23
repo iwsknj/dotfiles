f386b3824205ab83dedb08d239d208f3b694e4f3
iwsknj <kenji.iwase.5@gmail.com>
2024-02-24 03:12:07 +0900

    バックアップで取っていたnvimのファイル削除

--- lazynvim.bak/lua/plugins/coding.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-local nvim_cmp = {
-  "nvim-cmp",
-}
-
-table.insert(M, nvim_cmp)
-return M
--- nvim.bak/lua/appearance.lua
+++
@@ -1,4 +0,0 @@
-if not(vim.g.vscode) then
-    vim.cmd[[colorscheme tokyonight]]
-    vim.cmd[[highlight LineNr guifg=#87CEEB]] -- 行番号が見にくいので色を変えている
-end
