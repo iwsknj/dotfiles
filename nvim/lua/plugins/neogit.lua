3414a137868673737bd52bd2a4877737d3701606
iwsknj <kenji.iwase.5@gmail.com>
2024-03-19 12:06:39 +0900

    neogitのkeymap調整

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,12 +1,22 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim", -- required
+    "nvim-lua/plenary.nvim",  -- required
     "sindrets/diffview.nvim", -- optional - Diff integration
 
     -- Only one of these is needed, not both.
     "nvim-telescope/telescope.nvim", -- optional
-    "ibhagwan/fzf-lua", -- optional
+    "ibhagwan/fzf-lua",              -- optional
   },
-  config = true,
+  keys = {
+    {
+      "<leader>gn",
+      function()
+        local neogit = require("neogit")
+        neogit.open()
+      end,
+      desc = "Neogit",
+    },
+  },
+  config = true
 }
